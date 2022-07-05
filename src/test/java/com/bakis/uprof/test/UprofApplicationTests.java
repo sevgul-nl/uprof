30042727782febeba1dfeae7bf778b62fa6b2f8d
bakis-nl <baki.sevgul@gmail.com>
2022-07-05 10:49:47 +0200

    first commit

---
+++ src/test/java/com/bakis/uprof/test/UprofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakis.uprof.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class UprofApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
